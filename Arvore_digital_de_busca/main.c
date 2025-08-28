74b989f875c179e07f0baecbd4be76156134d984
Cauan Simões <cauan.simoes.contato@gmail.com>
2025-08-28 11:57:38 -0300

    Árvore digital de busca: adicionando comentários e funções auxiliares

--- Arvore_digital_de_busca/main.c
+++ Arvore_digital_de_busca/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define bits_na_chave 32
+#define bits_na_chave 4
 
+
+// estrutura do nó
 typedef struct no{
     unsigned chave;
     struct no *esq, *dir;
 } no;
 
+
+// função para criar o nó
 no* criarNo(unsigned chave)
 {
     no *novoNo = malloc(sizeof(no));
@@ -18,25 +22,35 @@ no* criarNo(unsigned chave)
     return novoNo;
 }
 
+// função para extrair um bit da chave com base no nivel
+// ex: 12 = 1100 e nivel = 2
+//   retorno = 0 (3 número)
+// lembrando que o valor de bits_na_chave é importante para entender qual será o bit de retorno
 unsigned bit(unsigned chave, int nivel)
 {
     return chave >> (bits_na_chave - 1 - nivel) & 1;
 }
 
+
+// função de busca
+// usa a função bit se não encontrar a chave
 no* busca_rec(no *raiz, unsigned chave, int nivel)
 {
+    // se a raiz é nula -> a chave não está na árvore
     if (raiz == NULL)
     {
         printf ("\nNão encontrado! - retornando null");
         return NULL;
     }
 
+    // chave encontrada -> retornando o nó
     if (chave == raiz->chave)
     {
         printf ("\nNumero encontrado!");
         return raiz;
     }
 
+    // buscando (pela esquerda ou direita) a chave
     if (bit(chave, nivel) == 0)
     {
         return busca_rec (raiz->esq, chave, nivel+1);
@@ -47,14 +61,21 @@ no* busca_rec(no *raiz, unsigned chave, int nivel)
     }
 }
 
+
+// função que chama a busca
 no* busca (no *raiz, unsigned chave)
 {
     return busca_rec(raiz, chave, 0);
 }
 
 
+
+// função para inserir uma chave na árvore digital de busca
+// usa a função bit para escolher o lado da inserção
 no* inserir_rec (no *raiz, unsigned chave, int nivel)
 {
+    
+    // se for nulo -> cria-se um novo nó
     if (raiz == NULL)
     {
         no *novoNo = criarNo(chave);
@@ -62,6 +83,7 @@ no* inserir_rec (no *raiz, unsigned chave, int nivel)
         return novoNo;
     }
 
+    // se a chave ja estiver no nó, apenas retorna
     if (chave == raiz->chave)
     {
         return raiz;
@@ -80,21 +102,31 @@ no* inserir_rec (no *raiz, unsigned chave, int nivel)
 }
 
 
+// função que chama a inserção
 no* inserir(no *raiz, unsigned chave)
 {
     return inserir_rec(raiz, chave, 0);
 }
 
-void imprimir (no *raiz)
+// função para imprimir a árvore (mesma impressão em_ordem da árvore ABB)
+// obviamente não vai imprimir em ordem crescente, pois a inserção não usa este critério
+void imprimir_em_ordem(no *raiz)
 {
     if (raiz != NULL)
     {
-        imprimir(raiz->esq);
+        imprimir_em_ordem(raiz->esq);
         printf ("\n%d", raiz->chave);
-        imprimir(raiz->dir);
+        imprimir_em_ordem(raiz->dir);
     }
 }
 
+// função para chamar a impressão
+void imprimir(no *raiz)
+{
+    printf ("\nIMPRIMINDO ARVORE!!\n");
+    imprimir_em_ordem(raiz);
+    printf ("\n\n");
+}
 
 int main ()
 {
